8141b3a6cce30ef9c9ccca507faf9c7c0f37fee7
Juan Luis Pérez <juanluis.perez.cabrera@bbva.com>
2020-12-10 16:24:29 +0100

    feature: implement logout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Login from './pages/Login'
+import MyHeader from './components/MyHeader'
+import { clearUser } from './actions/authedUser'
 
 class App extends Component {
-
+  logout() {
+    this.props.dispatch(clearUser())
+  }
   render() {
     return (
       <Router>
-          <div>
-              <Route path='/' exact component={Login} />
-          </div>
+        <MyHeader authorized={this.props.authedUser} onLogout={this.logout.bind(this)}/>
+        <div style={{ padding: '20px'}}>
+          <Route path='/' exact component={Login} />
+        </div>
       </Router>
     )
   }
 }
 
+function mapStateToProps ({  authedUser }) {
+  return {
+    authedUser: !! authedUser
+  }
+}
 
+export default connect(mapStateToProps)(App)
 
-export default App
 
--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,8 +1,15 @@
 export const SET_AUTHED_USER = 'SET_AUTHED_USER'
+export const CLEAR_USER = 'CLEAR_USER'
 
 export function setAuthedUser (id) {
   return {
     type: SET_AUTHED_USER,
     id,
+  }
+}
+
+export function clearUser () {
+  return {
+    type: CLEAR_USER
   }
 }
