64550ca22528e20ea66826b1854d4b18d5e879d0
Juan Luis Pérez <juanluis.perez.cabrera@bbva.com>
2020-12-11 12:22:31 +0100

    feature: develop MyCardQuestion

---
+++ src/components/MyCardQuestion.js
@@ -0,0 +1,23 @@
+const { default: MyButton } = require("./MyButton");
+
+const  MyCardQuestion = (({question, onClickButton}) => (
+<div className="col s12 m7">
+<h2 className="header">{`${question.user.name} said:`}</h2>
+    <div className="card horizontal">
+      <div className="card-image" style={{maxHeight: '100px', maxWidth: '100px'}}>
+        <img src={question.user.avatarURL} alt={question.user.name}/>
+      </div>
+      <div className="card-stacked">
+        <div className="card-content">
+            <span>What do you prefer : </span>
+            <p>1 . {question.optionOne.text} ?</p>
+            <p>2 . {question.optionTwo.text} ?</p>
+        </div>
+        <div className="card-action">
+          <MyButton onClick={() => onClickButton(question)} title="Go to result"/>
+        </div>
+      </div>
+    </div>
+</div>))
+
+export default MyCardQuestion
